8ce7d9b5b0ba90378486a67586f2c8bd2d83106e
anehing <anehing@gmail.com>
2014-03-19 17:27:29 +0800

    首页查询2条

--- models/post.js
+++ models/post.js
@@ -297,8 +297,8 @@ Post.getTen = function(name,page,callback){
 			collection.count(query,function(err,total){
 				//根据 query 对象查询，并跳过前 (page-1)*10 个结果，返回之后的 10 个结果
 				collection.find(query,{
-					skip: (page - 1)*10,
-					limit: 10
+					skip: (page - 1)*2,
+					limit: 2
 				}).sort({
 						time: -1
 				}).toArray(function(err,docs){
